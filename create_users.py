03a806a94ba3839958c75e781af6b26ac16cca55
kazuki.oodo <kazu2055.tennis@gmail.com>
2018-01-29 17:11:36 +0900

    add user create script

---
+++ create_users.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from django.contrib.auth.models import User
+
+user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+
+# At this point, user is a User object that has already been saved
+# to the database. You can continue to change its attributes
+# if you want to change other fields.
+user.last_name = 'Lennon'
+user.save()
