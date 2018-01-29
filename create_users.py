c3f0c7a1ea748f116ccbbf566edd7383a934d0b3
kazuki.oodo <kazu2055.tennis@gmail.com>
2018-01-29 17:18:45 +0900

    add permission of superuser to create user

--- create_users.py
+++ create_users.py
@@ -8,5 +8,6 @@ user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
 # At this point, user is a User object that has already been saved
 # to the database. You can continue to change its attributes
 # if you want to change other fields.
-user.last_name = 'Lennon'
+user.is_superuser=True
+user.is_staff=True
 user.save()
